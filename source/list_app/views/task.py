bf287803de068fea39b93c75ee8bfdf863027276
Alexandr Toropov <bligs100@gmail.com>
2022-09-23 00:39:27 +0600

    Файл task.py подключена обработка ошибки 404 вместо 500, в случае если передан неправильный pk

--- source/list_app/views/task.py
+++ source/list_app/views/task.py
@@ -1,6 +1,6 @@
 
 from http.client import HTTPResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponseNotFound
 from list_app.models import Task
 
@@ -14,24 +14,17 @@ def add_task_view(request):
     if request.method =='GET':
         context = {'choices': Choices.choices}
         return render(request, 'new_task.html', context=context)
-    print(request.POST.get('date'))
     task_data = {
         'description': request.POST.get('description'),
         'status': request.POST.get('status'),
         'deadline_date': request.POST.get('date')
     }
     task = Task.objects.create(**task_data)
-    print('redirect')
     return redirect(f'/')
 
 
 def display_task_view(request, pk):
-    # id = request.GET.get('id')
-    # task = Task.objects.get(pk=id)
-    try:
-        task = Task.objects.get(pk=pk)
-    except Task.DoesNotExist:
-        return HttpResponseNotFound("Not Found")
+    task = get_object_or_404(Task, pk=pk)
     context = {
         "task": task
     }
